7035638069051dcef2b7b6bd2ec7792f52075ec1
deynasaur <202110049@fit.edu.ph>
2024-09-20 19:58:24 +0800

    done

--- JS PROOF/mintNFTs.js
+++ JS PROOF/mintNFTs.js
@@ -9,24 +9,44 @@ Assessment Requirements
 */
 
 // create a variable to hold your NFT's
+const NFTs = [];
 
 // this function will take in some values as parameters, create an
 // NFT object using the parameters passed to it for its metadata, 
 // and store it in the variable above.
-function mintNFT () {
-
+function mintNFT (_name, _eyeColor, _shirtType, _bling) {
+    const NFT = {
+        "name": _name,
+        "eyeColor": _eyeColor,
+        "shirtType": _shirtType,
+        "bling": _bling
+    }
+    NFTs.push(NFT);
+    console.log("Minted: " + _name);
 }
 
 // create a "loop" that will go through an "array" of NFT's
 // and print their metadata with console.log()
 function listNFTs () {
-
+    for(let i = 0; i < NFTs.length; i++){
+        console.log("\nID: \t\t" + (i + 1));
+        console.log("Name: \t\t" + NFTs[i].name);
+        console.log("Eye Color: \t" + NFTs[i].eyeColor);
+        console.log("Shirt Type: " + NFTs[i].shirtType);
+        console.log("Bling: \t\t" + NFTs[i].bling);
+    }
 }
 
 // print the total number of NFTs we have minted to the console
 function getTotalSupply() {
-
+    console.log("\nTotal NFTs: " + NFTs.length);
 }
 
 // call your functions below this line
+mintNFT("Erica", "Blue", "Sweatshirt", "Silver Crucifix Necklace");
+mintNFT("Francheska", "Green", "T-shirt", "Gold Earrings");
+mintNFT("Xyrel", "Brown", "Hoodie", "Gold Chain");
+mintNFT("Liam", "Red", "Sweatshirt", "Silver Earrings");
 
+listNFTs();
+getTotalSupply();
